3c484c503710b9366b482d797d8d0935e24b295e
Dino Ratcliffe <yo@dino.io>
2018-04-10 08:33:43 +0100

    fixed polybar to work on multimonitor setups

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,4 +7,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-polybar example &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
