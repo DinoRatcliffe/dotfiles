68c9dcd80211bbf00d156a150acdfc85136bc0c7
Dino Ratcliffe <yo@dino.io>
2019-03-19 13:26:13 +0000

    added timer script that will be used for polybar task timer

---
+++ scripts/timer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+hour=0
+min=0
+sec=10
+name=""
+
+while getopts 'h:vm:vs:vn:v' flag; do
+    case "${flag}" in 
+        h) hour=${OPTARG} ;;
+        m) min=${OPTARG} ;;
+        s) sec=${OPTARG} ;;
+        n) name=${OPTARG} ;;
+    esac
+done
+
+while [ $hour -ge 0 ]; do
+        while [ $min -ge 0 ]; do
+                while [ $sec -ge 0 ]; do
+                        echo -ne "\033[2K"
+                        if [ ! -z ${name// } ]; then
+                            echo -ne "${name}: "
+                        fi
+                        if [ $hour -ne 0 ]; then
+                            echo -ne "${hour}h "
+                        fi
+                        if [ $min -ne 0 ]; then
+                            echo -ne "${min}m "
+                        fi
+                        echo -ne "${sec}s\r"
+                        let "sec=sec-1"
+                        sleep 1
+                done
+                sec=59
+                let "min=min-1"
+        done
+        min=59
+        let "hour=hour-1"
+done
